99f4f6e1fe7c779b876abffcd02f91b2d3f0e6ef
Dinuk Pathiraja <139424859+Pathiraja-D@users.noreply.github.com>
2023-12-20 23:09:21 +0530

    pages added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+
+export default function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/signin" element={<SignIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+    </Routes>
+    </BrowserRouter>;
+}
